8b0517e154bb1c5bb717e6056497fca99783f314
artyomdev250 <artyom.dev250@gmail.com>
2026-05-31 20:12:47 +0500

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
 
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Product from './pages/Product'
+
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
     </div>
   )
 }
