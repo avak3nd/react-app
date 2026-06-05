ed3cecfaa986feaff9441f6adbc7c653acd275bf
avak3nd <artyom.dev250@gmail.com>
2026-06-05 11:09:21 +0500

    trending and popular pages with components API

--- src/components/home/Popular.tsx
+++ src/components/home/Popular.tsx
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import type { Swiper as SwiperClass } from 'swiper';
 import { Link } from 'react-router-dom';
 import Card from '../ui/Card';
-import type { Game } from '../../types/game';
 
 import { ChevronRight, ChevronLeft } from '@boxicons/react';
 
@@ -13,85 +12,10 @@ import 'swiper/css/pagination';
 
 import { EffectCoverflow } from 'swiper/modules';
 
+import { useGamesByTag } from "../../hooks/useGamesByTag";
+
 function Popular() {
-    const [games] = useState<Game[]>([
-        {
-            _id: "1",
-            title: 'Grand Theft Auto V Enhanced',
-            img: "https://cdn1.epicgames.com/offer/b0cd075465c44f87be3b505ac04a2e46/GTAV_CHARM_Epic_FirstParty_PortraitFOB_1200x1600_R02_1200x1600-a5528b33df876e64f5dee728830c80a3",
-            type: 'Base game',
-            price: 49.99,
-        },
-        {
-            _id: "2",
-            title: 'VALORANT',
-            img: "https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S2_1200x1600-5b905ef8bbc4437fb4388ba8aa1f958d",
-            type: 'Base game',
-            price: 69.99,
-        },
-        {
-            _id: "3",
-            title: 'Hogwarts Legacy: Digital Deluxe Edition',
-            img: "https://cdn1.epicgames.com/offer/e97659b501af4e3981d5430dad170911/EGS_HogwartsLegacyDigitalDeluxeEdition_AvalancheSoftware_Editions_S2_1200x1600-1e3d53ba3c78a32a86fbadba7f4b9a58",
-            type: 'Base game',
-            price: 49.99,
-        },
-        {
-            _id: "4",
-            title: 'EA SPORTS FC™ 26 Standard Edition',
-            img: "https://cdn1.epicgames.com/offer/1d4d85b1051e41ee8f1a099e99d59f3f/EGS_EASPORTSFC26StandardEdition_EACANADA_S2_1200x1600-effee280c00b9890a0c5249d4b0e5c97",
-            type: 'Base game',
-            price: 59.99,
-        },
-        {
-            _id: "5",
-            title: 'Marvel Rivals',
-            img: "https://cdn1.epicgames.com/spt-assets/eb15454c010f4a748498cd3a62096a52/marvel-rivals-wq3mr.png",
-            type: 'Base game',
-            price: 39.99,
-        },
-        {
-            _id: "6",
-            title: 'Crosshair X',
-            img: "https://cdn1.epicgames.com/spt-assets/bd49b69c7a9e410b83b7c553c837a00a/crosshair-x-gr1xw.png",
-            type: 'Base game',
-            sale: 50,
-            price: 54.99,
-            salePrice: 27.49,
-        },
-        {
-            _id: "7",
-            title: 'STAR WARS™ Battlefront™ II: Celebration Edition',
-            img: "https://cdn1.epicgames.com/b156c3365a5b4cb9a01a5e1108b4e3f4/offer/EGS_STARWARSBattlefrontIICelebrationEdition_DICE_S2-1200x1600-11d040719a8457bbf36cabbe89b200db.jpg",
-            type: 'Base game',
-            sale: 65,
-            price: 69.99,
-            salePrice: 24.49,
-        },
-        {
-            _id: "8",
-            title: 'Farming Simulator 22',
-            img: "https://cdn1.epicgames.com/salesEvent/salesEvent/725818d8-9768-4a6a-a723-3039aaee1e23_1200x1600-3b6116d3fc1b566b71251d04bce27938",
-            type: 'Base game',
-            price: 59.99,
-        },
-        {
-            _id: "9",
-            title: 'Crosshair V2',
-            img: "https://cdn1.epicgames.com/spt-assets/836002e0e7e54201b3d4b68a84c3ee4c/crosshair-v2-jiemu.png",
-            type: 'Base game',
-            price: 39.99,
-        },
-        {
-            _id: "10",
-            title: 'LONESTAR',
-            img: "https://cdn1.epicgames.com/spt-assets/142bbd5f45fb4cf7a61b301d8a3b9900/lonestar-cggvr.jpg",
-            type: 'Base game',
-            sale: 50,
-            price: 54.99,
-            salePrice: 27.49,
-        },
-    ]);
+    const { data: games = [], isLoading, isError } = useGamesByTag("most_popular", 10);
 
     const swiperRef = useRef<SwiperClass | null>(null);
 
@@ -102,6 +26,23 @@ function Popular() {
         setIsBeginning(swiper.isBeginning);
         setIsEnd(swiper.isEnd);
     };
+
+    if (isLoading) {
+        return (
+            <div className="max-w-300 mx-auto text-white">
+                Loading...
+            </div>
+        );
+    }
+
+    if (isError) {
+        return (
+            <div className="max-w-300 mx-auto text-red-500">
+                Failed to load games
+            </div>
+        );
+    }
+
     return (
         <div className="max-w-300 mx-auto text-white">
             <div className='flex items-center justify-between'>
--- src/components/home/Trending.tsx
+++ src/components/home/Trending.tsx
@@ -1,57 +1,34 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Link } from 'react-router-dom';
 import Card from '../ui/Card';
-import type { Game } from '../../types/game';
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 
 import { EffectCoverflow } from 'swiper/modules';
-import { useState } from 'react';
+
+import { useGamesByTag } from "../../hooks/useGamesByTag";
 
 function Trending() {
-    const [games] = useState<Game[]>([
-        {
-            _id: "1",
-            title: 'The Bornless',
-            img: "https://cdn1.epicgames.com/spt-assets/2108caf3a9514ab0b1a4d2383197d452/the-bornless-j8etf.png",
-            type: 'Base game',
-            price: 49.99,
-        },
-        {
-            _id: "2",
-            title: 'LEGO® Batman™: Legacy of the Dark Knight',
-            img: "https://cdn1.epicgames.com/spt-assets/154c374195a443e9bf4e9fbd3a60d94f/rules-of-engagement-12qeu.jpg",
-            type: 'Base game',
-            price: 69.99,
-        },
-        {
-            _id: "3",
-            title: 'Retrace the Light',
-            img: "https://cdn1.epicgames.com/spt-assets/407fe6e2ec1040bfb596cd86b5512791/retracethelight-1xg61.png",
-            type: 'Base game',
-            price: 54.99,
-            sale: 50,
-            salePrice: 27.49,
-        },
-        {
-            _id: "4",
-            title: 'PROJECT_ORIGINS',
-            img: "https://cdn1.epicgames.com/spt-assets/d736723c491b4714980ee65c6ff9c3bb/project_origins-1d3tt.png",
-            type: 'Base game',
-            price: 69.99,
-            sale: 65,
-            salePrice: 24.49,
-        },
-        {
-            _id: "5",
-            title: 'Bunny Guys!',
-            img: "https://cdn1.epicgames.com/spt-assets/9022d4bbe9964858bfac21a499804b2d/bunny-guys-es0nf.jpg",
-            type: 'Base game',
-            price: 59.99,
-        }
-    ]);
+    const { data: games = [], isLoading, isError } = useGamesByTag("trending", 5);
+
+    if (isLoading) {
+        return (
+            <div className="max-w-300 mx-auto text-white">
+                Loading...
+            </div>
+        );
+    }
+
+    if (isError) {
+        return (
+            <div className="max-w-300 mx-auto text-red-500">
+                Failed to load games
+            </div>
+        );
+    }
+
     return (
         <div className="max-w-300 mx-auto text-white">
             <div className="flex items-center justify-between gap-4">
