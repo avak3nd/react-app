995dce5fbe06930930c5bf04d196c82a979965c5
avak3nd <artyom.dev250@gmail.com>
2026-06-06 11:38:18 +0500

    filter new styles

--- src/layouts/SearchBar.tsx
+++ src/layouts/SearchBar.tsx
@@ -74,7 +74,7 @@ function SearchBar({ rounded = false }: SearchBarProps) {
                             <Link
                                 key={game._id}
                                 to={`/product/${game._id}`}
-                                className="flex items-center gap-3 p-3 hover:bg-neutral-700 transition"
+                                className="flex items-center gap-4 py-3 px-4 hover:bg-neutral-700 transition"
                                 onClick={() =>
                                     setSearch("")
                                 }
@@ -85,8 +85,12 @@ function SearchBar({ rounded = false }: SearchBarProps) {
                                     className="w-12 h-16 object-cover rounded"
                                 />
                                 <div className="flex flex-col">
-                                    <span className="text-white text-sm">{game.title}</span>
-                                    <span className="text-white text-sm">{game.type}</span>
+                                    <p className="text-[13px] text-neutral-400 font-medium">
+                                        {game.type}
+                                    </p>
+                                    <p className="font-semibold text-[14px] mt-px text-white line-clamp-2 leading-5">
+                                        {game.title}
+                                    </p>
                                 </div>
                             </Link>
                         ))}
